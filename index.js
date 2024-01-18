3a85f45f2c82008ec7dfa857e36640764c5bcc39
assermoussa <asser.y.moussa@gmail.com>
2024-01-18 16:50:53 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1*num2;
 
-// Write your code here
+let random =  Math.ceil(Math.random());
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = 1;
+max = Math.max(1, 2, 3, 4, 5, 20);
